5f3bf94bc23fdfb34377253b83bfe04e54682102
LEEYUJOON <123285545+L-U-Ready@users.noreply.github.com>
2025-08-29 22:05:14 +0900

    first commit

---
+++ src/main/java/org/javaspringdddenvironment/JavaSpringDddEnvironmentApplication.java
@@ -0,0 +1,13 @@
+package org.javaspringdddenvironment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaSpringDddEnvironmentApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JavaSpringDddEnvironmentApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/javaspringdddenvironment/JavaSpringDddEnvironmentApplicationTests.java
@@ -0,0 +1,13 @@
+package org.javaspringdddenvironment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaSpringDddEnvironmentApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
